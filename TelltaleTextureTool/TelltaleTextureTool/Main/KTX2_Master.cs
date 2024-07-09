1eb91232a4a661f66482bbf4d4c9a9fa1eaeb00c
DarkShadow <ivanivov.p@gmail.com>
2024-07-10 00:04:51 +0300

    Add experimental KTX2 support

--- TelltaleTextureTool/TelltaleTextureTool/Main/KTX2_Master.cs
+++ TelltaleTextureTool/TelltaleTextureTool/Main/KTX2_Master.cs
@@ -4,82 +4,170 @@ using System.IO;
 using TelltaleTextureTool.DirectX;
 using System.Linq;
 using static Ktx.Ktx2;
+using TelltaleTextureTool.TelltaleEnums;
+using Ktx;
+using TelltaleTextureTool.Telltale.FileTypes.D3DTX;
+using TelltaleTextureTool.Utilities;
 
 namespace TelltaleTextureTool.Main
 {
     /// <summary>
     /// Main class for managing D3DTX files and converting them to KTX2.
     /// </summary>
-    public class KTX2_Master
+    public unsafe class KTX2_Master
     {
-        TextureCreateInfo ktx2Texture;
+        TextureCreateInfo ktx2TextureInfo;
+        Texture* texture;
 
-        // /// <summary>
-        // /// Create a KTX2 file from a D3DTX.
-        // /// </summary>
-        // /// <param name="d3dtx">The D3DTX data that will be used.</param>
-        // unsafe public Texture* InitializeTextureHeader(D3DTX_Master d3dtx)
-        // {
-        //     if (d3dtx.IsLegacyD3DTX())
-        //     {
-        //         throw new NotImplementedException("KTX2 does not support legacy versions.");
-        //     }
+        public byte[] header = [];
+        public byte[] pixelData = [];
 
-        //     // Initialize the KTX2 Header
-        //     ErrorCode error = Create(new TextureCreateInfo()
-        //     {
-        //         BaseHeight = (uint)d3dtx.GetHeight(),
-        //         BaseWidth = (uint)d3dtx.GetWidth(),
-        //         BaseDepth = (uint)d3dtx.GetDepth(),
-        //         VkFormat = KTX2_HELPER.GetVkFormatFromTelltaleSurfaceFormat(d3dtx.GetCompressionType(), d3dtx.GetSurfaceGamma(), d3dtx.GetAlpha()),
-        //         NumLevels = (uint)d3dtx.GetMipMapCount(),
-        //         NumLayers = (uint)d3dtx.GetArraySize(),
-        //         NumFaces = (uint)(d3dtx.IsCubeTexture() ? 6 : 1),
-        //         IsArray = (byte)(d3dtx.IsArrayTexture() ? 1 : 0),
-        //         NumDimensions = (uint)(d3dtx.IsVolumeTexture() ? 3 : 2)
-        //     },
+        /// <summary>
+        /// Create a DDS file from a D3DTX.
+        /// </summary>
+        /// <param name="d3dtx">The D3DTX data that will be used.</param>
+        public KTX2_Master(D3DTX_Master d3dtx)
+        {
+            if (d3dtx.HasDDSHeader())
+            {
+                return;
+            }
 
-        //     TextureCreateStorage.AllocStorage, out Texture* test);
+            InitializeKTX2Header(d3dtx);
+            InitializeKTX2PixelData(d3dtx);
+        }
 
+        private void InitializeKTX2Header(D3DTX_Master d3dtx)
+        {
+            D3DTXMetadata d3dtxMetadata = d3dtx.GetMetadata();
 
-        //     List<byte[]> textureData = [];
+            Console.WriteLine("D3dtx width: " + d3dtxMetadata.Width);
+            Console.WriteLine("D3dtx height: " + d3dtxMetadata.Height);
+            Console.WriteLine("D3dtx mip map count: " + d3dtxMetadata.MipLevels);
+            Console.WriteLine("D3dtx d3d format: " + d3dtxMetadata.D3DFormat);
 
-        //     // Get all pixel data from the D3DTX
-        //     var d3dtxTextureData = d3dtx.GetPixelData();
+            T3SurfaceFormat surfaceFormat = d3dtxMetadata.Format;
+            T3SurfaceGamma surfaceGamma = d3dtxMetadata.SurfaceGamma;
+            T3PlatformType platformType = d3dtxMetadata.Platform;
+            T3TextureAlphaMode alphaMode = d3dtxMetadata.AlphaMode;
 
-        //     int divideBy = 1;
+            ktx2TextureInfo = new()
+            {
+                BaseWidth = d3dtxMetadata.Width,
+                BaseHeight = d3dtxMetadata.Height,
+                BaseDepth = d3dtxMetadata.Depth,
+                NumLevels = d3dtxMetadata.MipLevels,
+                NumLayers = d3dtxMetadata.ArraySize,
+                NumFaces = (uint)(d3dtxMetadata.IsCubemap() ? 6 : 1),
+                NumDimensions = (uint)(d3dtxMetadata.IsVolumemap() ? 3 : 2),
+                IsArray = (byte)(d3dtxMetadata.IsArrayTexture() ? 1 : 0),
+                GenerateMipmaps = (byte)(d3dtxMetadata.MipLevels > 1 ? 1 : 0),
+                VkFormat = KTX2_HELPER.GetVkFormatFromTelltaleSurfaceFormat(surfaceFormat, surfaceGamma, platformType, alphaMode)
+            };
 
-        //     // Get the pixel data by mipmap levels and unswizzle depending on the Platform
-        //     for (int i = (int)(d3dtx.GetMipMapCount() - 1); i >= 0; i--)
-        //     {
-        //         textureData.Add(d3dtx.GetPixelDataByMipmapIndex(i, d3dtx.GetCompressionType(), (int)d3dtx.GetWidth() / divideBy, (int)d3dtx.GetHeight() / divideBy, d3dtx.GetPlatformType()));
-        //         divideBy *= 2;
-        //     }
+            if (D3DTX_Master.IsFormatIncompatibleWithDDS(surfaceFormat))
+            {
+                ktx2TextureInfo.NumLevels = 1;
+                ktx2TextureInfo.GenerateMipmaps = 0;
+            }
 
-        //     byte[] d3dtxTextureDataArray = textureData.SelectMany(b => b).ToArray();
+            Ktx2.ErrorCode err = Ktx2.Create(ktx2TextureInfo, Ktx2.TextureCreateStorage.AllocStorage, out texture);
 
-        //     // // Attempt to put pixel data into the Texture
-        //     return test;
-        // }
+            if (err != Ktx2.ErrorCode.Success)
+            {
+                throw new Exception("Failed to create KTX2 texture.");
+            }
+        }
 
-        // /// <summary>
-        // /// Writes a D3DTX into a KTX2 file on the disk.
-        // /// </summary>
-        // /// <param name="d3dtx"></param>
-        // /// <param name="destinationDirectory"></param>
-        // unsafe public void WriteD3DTXAsKTX2(D3DTX_Master d3dtx, string destinationDirectory)
-        // {
-        //     var watch = System.Diagnostics.Stopwatch.StartNew();
-        //     string d3dtxFilePath = d3dtx.filePath;
-        //     string fileName = Path.GetFileNameWithoutExtension(d3dtxFilePath);
+        private void InitializeKTX2PixelData(D3DTX_Master d3dtx)
+        {
+            // NOTES: Telltale mip levels are reversed in Poker Night 2 and above. The first level are the smallest mip levels and the last level is the largest mip level.
+            // The faces are NOT reverse.
+            // This is likely corelating with the way that KTX and KTX2 files are written.
+            // Some normal maps specifically with type 4 (eTxNormalMap) channels are all reversed (ABGR instead of RGBA) (Only applies for newer games)
+            // Some surface formats are dependant on platforms. For example, iOS textures have their R and B channels swapped.
+            // Some surface formats are not supported by DDS. In this case, the texture will be written as a raw texture.
 
-        //     string newKTX2Path = destinationDirectory + Path.DirectorySeparatorChar + fileName +
-        //                                                Main_Shared.ktx2Extension;
+            if (d3dtx.IsLegacyD3DTX())
+            {
+                throw new Exception("KTX2 does not support legacy D3DTX files.");
 
-        //   //  Ktx2.WriteToNamedFile(texture, newKTX2Path);
-        //     watch.Stop();
-        //     var elapsedMs = watch.ElapsedMilliseconds;
-        //     Console.WriteLine("Time to get data: {0}", elapsedMs);
-        // }
+            }
+
+            D3DTXMetadata metadata = d3dtx.GetMetadata();
+
+            T3SurfaceFormat surfaceFormat = metadata.Format;
+
+            List<byte[]> textureData = d3dtx.GetPixelData();
+
+            textureData.Reverse();
+
+            for (int i = 0; i < ktx2TextureInfo.NumLevels; i++)
+            {
+                for (int j = 0; j < ktx2TextureInfo.NumLayers; j++)
+                {
+                    for (int k = 0; k < ktx2TextureInfo.NumFaces; k++)
+                    {
+                        Ktx2.ErrorCode err = Ktx2.SetImageFromMemory(texture, (uint)i, (uint)j, (uint)k, textureData[i + j + k][0], (nuint)textureData[i + j + k].Length);
+
+                        if (err != Ktx2.ErrorCode.Success)
+                        {
+                            throw new Exception("Failed to set image from memory.");
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Writes a D3DTX into a KTX2 file on the disk.
+        /// </summary>
+        /// <param name="d3dtx"></param>
+        /// <param name="destinationDirectory"></param>
+        public void WriteD3DTXAsKTX2(D3DTX_Master d3dtx, string destinationDirectory)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            string d3dtxFilePath = d3dtx.FilePath;
+            string fileName = Path.GetFileNameWithoutExtension(d3dtxFilePath);
+
+            string newKTX2Path = destinationDirectory + Path.DirectorySeparatorChar + fileName +
+                                                       Main_Shared.ktx2Extension;
+
+            Ktx2.WriteToNamedFile(texture, newKTX2Path);
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("Time to get data: {0}", elapsedMs);
+        }
+
+        /// <summary>
+        /// Get the correct DDS data from a Telltale texture.
+        /// </summary>
+        /// <param name="d3dtx"></param>
+        /// <returns></returns>
+        public byte[] GetData(D3DTX_Master d3dtx)
+        {
+            Console.WriteLine("Getting data for: " + d3dtx.FilePath);
+
+            // If the D3DTX exists, return the pixel data.
+            if (d3dtx.d3dtxObject == null)
+            {
+                throw new InvalidDataException("There is no pixel data to be written.");
+            }
+
+            // If the D3DTX has a DDS header, return the whole pixel data.
+            if (d3dtx.HasDDSHeader())
+            {
+                return d3dtx.GetPixelData()[d3dtx.GetPixelData().Count - 1];
+            }
+
+            // Return the created DDS file.
+            return ByteFunctions.Combine(header, pixelData);
+        }
+
+        ~KTX2_Master()
+        {
+            Destroy(texture);
+        }
     }
 }
+
