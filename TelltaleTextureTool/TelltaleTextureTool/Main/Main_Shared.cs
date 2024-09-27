55de7ef514cbef5a3c3cf8bb034dfd9ee2656d84
DarkShadow <ivanivov.p@gmail.com>
2024-09-27 18:17:29 +0300

    Make extensions strings constants

--- TelltaleTextureTool/TelltaleTextureTool/Main/Main_Shared.cs
+++ TelltaleTextureTool/TelltaleTextureTool/Main/Main_Shared.cs
@@ -3,17 +3,17 @@
 public class Main_Shared
 {
     // File extensions
-    public static string d3dtxExtension = ".d3dtx";
-    public static string ddsExtension = ".dds";
-    public static string ktxExtension = ".ktx";
-    public static string ktx2Extension = ".ktx2";
-    public static string jsonExtension = ".json";
-    public static string bmpExtension = ".bmp";
-    public static string pngExtension = ".png";
-    public static string jpegExtension = ".jpeg";
-    public static string jpgExtension = ".jpg";
-    public static string tiffExtension = ".tiff";
-    public static string tifExtension = ".tif";
-    public static string tgaExtension = ".tga";
-    public static string hdrExtension = ".hdr";
+    public const string d3dtxExtension = ".d3dtx";
+    public const string ddsExtension = ".dds";
+    public const string ktxExtension = ".ktx";
+    public const string ktx2Extension = ".ktx2";
+    public const string jsonExtension = ".json";
+    public const string bmpExtension = ".bmp";
+    public const string pngExtension = ".png";
+    public const string jpegExtension = ".jpeg";
+    public const string jpgExtension = ".jpg";
+    public const string tiffExtension = ".tiff";
+    public const string tifExtension = ".tif";
+    public const string tgaExtension = ".tga";
+    public const string hdrExtension = ".hdr";
 }
